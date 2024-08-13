09a1196b2781619760bf034fcb36a471c42ecb7d
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-08-13 19:50:29 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import Time from './components/Time/Time';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="App">
-      
-      <Time/>
-    </div>
+     
+     <Time/>
+   </div>
+    </>
+
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
+import './Navbar.css'
+
 const Navbar = () =>{
     return(
-        <div>
+        <div className="header">
             <div>
-                <h1>Piyush Savale</h1>
+                <h1 className='heading-1'>GetYourWorkDoneApp</h1>
+            </div>
+
+            <div>
+                <img className="img"src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/>
             </div>
         </div>
     )
